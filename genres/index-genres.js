6c91a3144a24a70088ae84f8fe46a3c5c4c0e931
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-04-22 16:23:07 +0200

    first commit

---
+++ genres/index-genres.js
@@ -0,0 +1,10 @@
+fetch("https://devsapihub.com/api-movies").then(function (response) {
+  return response.json();}).then (function (data) {
+          for(let i = 0; i < data.length; i++){
+          if(data[i].genre == "Fantasy"){
+          console.log(data[i].title, data[i].image_url, data[i].genre, data[i].stars, data[i].year);
+          document.getElementById("mov").innerHTML += `<h2>${data[i].title}</h2>
+                                                      <img src=${data[i].image_url}></div>
+                                                      <p>${data[i].genre}</p>
+                                                      <p>${data[i].stars}</p>
+                                                      <p>${data[i].year}</p>`}}});
