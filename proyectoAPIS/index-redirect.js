6178aff702e36e310c6bc12d78568cbee70aaee7
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-05-10 18:00:52 +0200

    Delete proyectoAPIS directory

--- proyectoAPIS/index-redirect.js
+++
@@ -1,5 +0,0 @@
-let actualMovies =  ["https://www.youtube.com/watch?v=IuSwvdIsM_E", "https://www.youtube.com/watch?v=E05pI-sXI7g", "https://www.youtube.com/watch?v=s6OoKCgTxCY", "https://www.youtube.com/watch?v=br5eTcMm6hM", "https://www.youtube.com/watch?v=fvx_GX1oK_E"]
-
-
-
-    document.getElementById("embededContent").innerHTML = `<embed src="${actualMovies[Math.random()*5]}"></embed>`
